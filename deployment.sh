61121aa7ea53bdb0a5d641fd4d9ffd6a51314530
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2019-10-31 23:18:49 -0400

    added deployment.sh, deployment successful

---
+++ deployment.sh
@@ -0,0 +1,37 @@
+export CALLBACK_URL="https://flip.mauridev.net"
+export DOMAIN_NAME="auth.mauridev.net"
+export CERTIFICATE="arn:aws:acm:us-east-1:972073858291:certificate/bdf31f78-5e34-4bf7-a098-075b39f205ec"
+export STACK="authorizer"
+
+if [ $1 = "-c" ] || [ $1 = "--create" ]
+then
+
+aws cloudformation create-stack \
+  	--stack-name $STACK \
+	--template-body file://template.yaml \
+	--capabilities CAPABILITY_AUTO_EXPAND \
+	--parameters \
+		ParameterKey=DomainName,ParameterValue=$DOMAIN_NAME \
+		ParameterKey=Certificate,ParameterValue=$CERTIFICATE \
+		ParameterKey=CallbackURL,ParameterValue=$CALLBACK_URL
+
+fi
+
+if [ $1 = "-u" ] || [ $1 = "--update" ]
+then
+
+aws cloudformation update-stack \
+  	--stack-name flip-it-web \
+	--template-body file://template.yaml \
+	--capabilities CAPABILITY_AUTO_EXPAND \
+	--parameters \
+		ParameterKey=DomainName,ParameterValue=$DOMAIN_NAME \
+		ParameterKey=Certificate,ParameterValue=$CERTIFICATE \
+		ParameterKey=CallbackURL,ParameterValue=$CALLBACK_URL
+
+fi
+
+if [ $1 = "-r" ] || [ $1 = "--remove" ]
+then
+  aws cloudformation delete-stack --stack-name $STACK
+fi
