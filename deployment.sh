03577bac0067746b26b0787a619a59edb81792eb
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2019-11-03 01:52:02 -0400

    added custom domain configuration

--- deployment.sh
+++ deployment.sh
@@ -1,8 +1,9 @@
 export CALLBACK_URL="https://flip.mauridev.net"
 export DOMAIN_NAME="auth.mauridev.net"
 export USERS_BUCKET="apps-user-storage"
-export CERTIFICATE="arn:aws:acm:us-east-1:972073858291:certificate/bdf31f78-5e34-4bf7-a098-075b39f205ec"
+export CERTIFICATE="arn:aws:acm:us-east-1:972073858291:certificate/6a8da1db-a6a8-4ace-8855-e2ee47da58ea"
 export STACK="authorizer"
+export HOSTED_ZONE_ID="Z1IUAUSKRI6O7T"
 
 if [ $1 = "-c" ] || [ $1 = "--create" ]
 then
@@ -15,7 +16,8 @@ aws cloudformation create-stack \
 		ParameterKey=DomainName,ParameterValue=$DOMAIN_NAME \
 		ParameterKey=Certificate,ParameterValue=$CERTIFICATE \
 		ParameterKey=CallbackURL,ParameterValue=$CALLBACK_URL \
-		ParameterKey=UsersBucket,ParameterValue=$USERS_BUCKET
+		ParameterKey=UsersBucket,ParameterValue=$USERS_BUCKET \
+		ParameterKey=HostedZoneId,ParameterValue=$HOSTED_ZONE_ID
 
 fi
 
@@ -23,14 +25,15 @@ if [ $1 = "-u" ] || [ $1 = "--update" ]
 then
 
 aws cloudformation update-stack \
-  	--stack-name flip-it-web \
+  	--stack-name $STACK \
 	--template-body file://template.yaml \
 	--capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM \
 	--parameters \
 		ParameterKey=DomainName,ParameterValue=$DOMAIN_NAME \
 		ParameterKey=Certificate,ParameterValue=$CERTIFICATE \
 		ParameterKey=CallbackURL,ParameterValue=$CALLBACK_URL \
-		ParameterKey=UsersBucket,ParameterValue=$USERS_BUCKET
+		ParameterKey=UsersBucket,ParameterValue=$USERS_BUCKET \
+		ParameterKey=HostedZoneId,ParameterValue=$HOSTED_ZONE_ID
 
 fi
 
