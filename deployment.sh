60c07a5f78ce33eeac659962e2193e6be558ce29
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2019-11-02 23:38:52 -0400

    added identity pool, s3 bucket  and role for authenticated users

--- deployment.sh
+++ deployment.sh
@@ -1,5 +1,6 @@
 export CALLBACK_URL="https://flip.mauridev.net"
 export DOMAIN_NAME="auth.mauridev.net"
+export USERS_BUCKET="apps-user-storage"
 export CERTIFICATE="arn:aws:acm:us-east-1:972073858291:certificate/bdf31f78-5e34-4bf7-a098-075b39f205ec"
 export STACK="authorizer"
 
@@ -9,11 +10,12 @@ then
 aws cloudformation create-stack \
   	--stack-name $STACK \
 	--template-body file://template.yaml \
-	--capabilities CAPABILITY_AUTO_EXPAND \
+	--capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM \
 	--parameters \
 		ParameterKey=DomainName,ParameterValue=$DOMAIN_NAME \
 		ParameterKey=Certificate,ParameterValue=$CERTIFICATE \
-		ParameterKey=CallbackURL,ParameterValue=$CALLBACK_URL
+		ParameterKey=CallbackURL,ParameterValue=$CALLBACK_URL \
+		ParameterKey=UsersBucket,ParameterValue=$USERS_BUCKET
 
 fi
 
@@ -23,11 +25,12 @@ then
 aws cloudformation update-stack \
   	--stack-name flip-it-web \
 	--template-body file://template.yaml \
-	--capabilities CAPABILITY_AUTO_EXPAND \
+	--capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM \
 	--parameters \
 		ParameterKey=DomainName,ParameterValue=$DOMAIN_NAME \
 		ParameterKey=Certificate,ParameterValue=$CERTIFICATE \
-		ParameterKey=CallbackURL,ParameterValue=$CALLBACK_URL
+		ParameterKey=CallbackURL,ParameterValue=$CALLBACK_URL \
+		ParameterKey=UsersBucket,ParameterValue=$USERS_BUCKET
 
 fi
 
